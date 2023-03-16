12b174d4270c759cc629856c331df1440fd1b69e
WilliamAU <202006324@post.au.dk>
2023-03-16 13:51:26 +0100

    Ported over utils from old project

---
+++ src/main/java/application/utils/ConsoleUtils.java
@@ -0,0 +1,23 @@
+package application.utils;
+
+import java.io.IOException;
+
+public class ConsoleUtils {
+    public static void clearConsole() {
+        try {
+            String operatingSystem = System.getProperty("os.name");
+            if (operatingSystem.contains("Windows")) {
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+                startProcess.waitFor();
+            }
+            System.out.println();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
